a5b2c9aa789ea71e8888ce1fac7c0c5d5ca70273
Alena Mordvinceva <a.mordvinceva@andersenlab.com>
2017-01-24 11:57:03 +0300

    delete module

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,3 @@ export { AutoComplete } from './auto-complete';
 export { AutoCompleteComponent } from './auto-complete.component';
 export { AutoCompleteDirective } from './auto-complete.directive';
 
-export { Ng2AutoCompleteModule } from './ng2AutoComplete.module';
